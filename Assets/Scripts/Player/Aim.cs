9fd5cd58e7d9aef54df5d08f399878896c2e0fc9
Fran <xiisku@gmail.com>
2016-02-26 14:58:29 +0100

    Flip and Some graphics
    Flip player and gun and sprites

--- Assets/Scripts/Player/Aim.cs
+++ Assets/Scripts/Player/Aim.cs
@@ -5,6 +5,23 @@ public class Aim : MonoBehaviour {
 
 	public float speed;
 
+	public Transform Trans;
+
+	public Transform RenderTacoman;
+
+	public Transform RenderGun;
+
+	public bool facingRight;
+
+	public float rotationz;
+
+	public Vector2 newScaleGun;
+	public Vector2 newScaleTaco;
+	void Start(){
+
+		facingRight = true;
+	}
+
 	void FixedUpdate(){
 		var mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		Quaternion rot = Quaternion.LookRotation (transform.position - mousePos, Vector3.forward);
@@ -13,6 +30,46 @@ public class Aim : MonoBehaviour {
 
 		transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z);
 
+		/*
+		if (move > 0 && facingRight) Flip ();
+		else if (move < 0 && !facingRight) Flip ();
+
+
+		if (Trans.localRotation.eulerAngles >= new Vector3(180,0,0)) {
+		
+			if(RenderTacoman.transform.localScale.x >= 0){
+
+				RenderTacoman.transform.localScale.x *= -1;
+				RenderGun.transform.localScale.x *= -1;
+			}
+
+		
+		}*/
+		rotationz = Trans.eulerAngles.z;
+
+		if ((rotationz >= 180)&&facingRight) Flip();
+		else if (rotationz <= 180 && !facingRight) Flip ();
 
 	}
+
+	public void Flip(){
+
+		newScaleGun = RenderGun.localScale;
+		
+		newScaleGun.y *= -1;
+		
+		RenderGun.localScale = newScaleGun;
+
+		newScaleTaco = RenderTacoman.localScale;
+		
+		newScaleTaco.x *= -1;
+		
+		RenderTacoman.localScale = newScaleTaco;
+		
+		facingRight = !facingRight;
+
+
+
+	}
+
 }
