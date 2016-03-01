d26a68700792dbce86ddae1b0f8d8acc1fe58528
Fran <xiisku@gmail.com>
2016-03-01 12:17:50 +0100

    test

--- Assets/ShadowCreation.cs
+++ Assets/ShadowCreation.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class ShadowCreation : MonoBehaviour {
 
+	public GameObject Shadow;
+
 	// Use this for initialization
 	void Start () {
 	
+
+
 	}
 	
 	// Update is called once per frame
